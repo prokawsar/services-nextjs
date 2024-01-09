26b27e788d02970ff4724b17c79bde9b9e690627
prokawsar <prokawsar@gmail.com>
2024-01-09 17:41:16 +0600

    fix footer

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
     <div className="bg-slate-50 w-full flex justify-center">
       <div className="flex flex-row w-full mx-auto max-w-6xl justify-between py-3">
         <div className="flex items-center">
-          <p className="">Made by Kawsar with Next.js</p>
+          <p className="">Made by Kawsar with Next.js | 2024</p>
         </div>
         <div className="flex flex-row items-center">
           <Link
-            className="rounded-md p-2 hover:bg-gray-100 hover:font-semibold duration-150"
+            className="rounded-md p-2 hover:bg-slate-100/60 duration-150"
             href={'/'}
           >
             Terms and Conditions
