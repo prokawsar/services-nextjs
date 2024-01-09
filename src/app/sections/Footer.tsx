b0dae669f1de4f58f52cbf5e77d67334333fa77e
prokawsar <prokawsar@gmail.com>
2024-01-09 17:58:16 +0600

    b

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div className="bg-slate-50 w-full flex justify-center">
+    <div className="bg-slate-50 w-full flex justify-center border-t-2 border-slate-200">
       <div className="flex flex-row w-full mx-auto max-w-6xl justify-between py-3">
         <div className="flex items-center">
           <p className="">Made by Kawsar with Next.js | 2024</p>
