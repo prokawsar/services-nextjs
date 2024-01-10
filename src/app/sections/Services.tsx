f67c2621b17f26762175146542a82286a920fb51
prokawsar <prokawsar@gmail.com>
2024-01-10 16:28:08 +0600

    set bg

--- src/app/sections/Services.tsx
+++ src/app/sections/Services.tsx
@@ -4,9 +4,9 @@ export default function Services() {
   return (
     <div
       id="services"
-      className="flex w-full max-w-6xl h-96 items-center mx-auto py-10"
+      className="flex w-full h-96 justify-center bg-gray-50 items-center mx-auto py-10"
     >
-      <div className="flex flex-row gap-3 w-full justify-between">
+      <div className="flex  max-w-6xl flex-row gap-3 w-full justify-between">
         <Card
           className="flex-1 items-center justify-center"
           title="Web Development"
